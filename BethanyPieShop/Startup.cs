a8a86d76a9bcbb98d3ab1eba35962d367d1f257c
SudeepChaulagain <sudeepchaulagain7777@gmail.com>
2021-04-21 09:37:31 +0545

    Configuration of EF core

--- BethanyPieShop/Startup.cs
+++ BethanyPieShop/Startup.cs
@@ -6,6 +6,8 @@ using BethanyPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,6 +17,14 @@ namespace BethanyPieShop
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -25,6 +35,9 @@ namespace BethanyPieShop
             //this allows to use MVC pattern in the application
             services.AddControllersWithViews();
 
+            //for entity framework
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
            
         }
 
