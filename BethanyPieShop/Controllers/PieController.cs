af3c2868e2174665ff4919a361934ea49387aeef
SudeepChaulagain <sudeepchaulagain7777@gmail.com>
2021-04-23 08:45:04 +0545

    Added navigation bar and details view

--- BethanyPieShop/Controllers/PieController.cs
+++ BethanyPieShop/Controllers/PieController.cs
@@ -30,5 +30,13 @@ namespace BethanyPieShop.Controllers
             return View(piesListViewModel);
         }
 
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+            
+        }
     }
 }
